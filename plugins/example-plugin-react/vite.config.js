34d458d1468e50167585708bc49a8374e81bd6cc
kasunben <mailtokasun@gmail.com>
2025-10-25 12:23:54 +0200

    configure react build to serve the js bundel only

--- plugins/example-plugin-react/vite.config.js
+++ plugins/example-plugin-react/vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.jsx"),
+      formats: ["iife"],
+      fileName: () => "index.js",
+      // REQUIRED for iife/umd builds:
+      name: "ExamplePluginReact",
+    },
+    rollupOptions: {
+      output: { inlineDynamicImports: true },
+    },
+    outDir: "dist",
+    assetsDir: "",
+    emptyOutDir: true,
+  },
+  // rollupOptions: {
+  //   external: ["react", "react-dom"],
+  //   output: {
+  //     inlineDynamicImports: true,
+  //     globals: {
+  //       react: "React",
+  //       "react-dom": "ReactDOM",
+  //     },
+  //   },
+  // },
   server: {
     port: 4002,
   },
@@ -11,3 +42,9 @@ export default defineConfig({
     port: 8002,
   },
 });
+
+/**
+ * TODO: Uncomment rollupOptions, once configured platfrom to include react deps by default.
+ * <script src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+ * <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+ */
